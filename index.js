6d5264e2a518a295abec61b1baa3f3fec3e0933f
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-08-02 21:05:59 +0200

    upd3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const contacts = require('./db/contacts.json');
+const contacts = require("./db/contacts");
+// const contacts = require(".contacts");
+
+contacts
+  .listContacts()
+  .then((contact) => console.log(contact))
+  .catch((err) => console.log(err));
